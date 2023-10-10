cfc27f89b7a8bc7983dae031fe92672403ab8815
Gustavo Canellas Aveline Rocha <125305894+GustavoCanellasAvelineRocha@users.noreply.github.com>
2023-10-09 21:22:42 -0300

    first commit

---
+++ src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
