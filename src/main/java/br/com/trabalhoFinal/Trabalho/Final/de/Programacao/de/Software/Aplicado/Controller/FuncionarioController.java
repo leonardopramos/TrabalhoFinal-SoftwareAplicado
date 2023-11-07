7232e70ce67675e11fbf35c51c4c992d80d24d23
Gustavo Canellas Aveline Rocha <125305894+GustavoCanellasAvelineRocha@users.noreply.github.com>
2023-11-06 21:12:32 -0300

    nao sei

--- src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/Controller/FuncionarioController.java
+++ src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/Controller/FuncionarioController.java
@@ -1,12 +1,17 @@
 package br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.Controller;
 
 import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.funcionario.Funcionario;
-import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.funcionario.FuncionarioRepository;
 import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.funcionario.FuncionarioService;
+import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.reembolso.Estado;
+import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.reembolso.Motivo;
+import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.reembolso.Reembolso;
+import br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.reembolso.ReembolsoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/funcionario")
@@ -14,6 +19,9 @@ public class FuncionarioController {
 
     @Autowired
     FuncionarioService funcionarioService;
+
+    @Autowired
+    ReembolsoService reembolsoService;
 
     @PostMapping("/cadastro")
     public ResponseEntity cadastroFuncionario(@RequestBody String nome, String senha){
@@ -29,15 +37,22 @@ public class FuncionarioController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/cadastro-reembolso")
-    public ResponseEntity cadastroReembolso(){
+    @PostMapping("/reembolso")
+    public ResponseEntity cadastroReembolso(@RequestBody Double valor, Motivo motivo, Estado estado, LocalDate dataReembolso){
+        Reembolso reembolso = Reembolso.builder()
+                .valor(valor)
+                .motivo(motivo)
+                .estado(estado)
+                .dataReembolso(dataReembolso)
+                .build();
 
+        reembolsoService.cadastraReembolso(reembolso);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @GetMapping("/cadastro-reembolso")
+    @GetMapping("/reembolso")
     public ResponseEntity visualizaReembolsos(){
-
+        
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/domain/reembolso/ReembolsoService.java
+++ src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/domain/reembolso/ReembolsoService.java
@@ -1,11 +1,28 @@
 package br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado.domain.reembolso;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReembolsoService {
 
-    public void cadastraReembolso(){
+    @Autowired
+    ReembolsoRepository reembolsoRepository;
 
+    public boolean valida(Reembolso reembolso){
+        if(reembolso.valor < 0)throw new RuntimeException();;
+
+        if(reembolso.estado == null) throw new RuntimeException();
+
+        if(reembolso.motivo == null) throw new RuntimeException();
+
+        if(reembolso.dataReembolso == null)throw new RuntimeException();
+
+        return true;
+    }
+
+    public void cadastraReembolso(Reembolso reembolso){
+        if(valida(reembolso)) reembolsoRepository.save(reembolso);
+        else throw new RuntimeException();
     }
 }
