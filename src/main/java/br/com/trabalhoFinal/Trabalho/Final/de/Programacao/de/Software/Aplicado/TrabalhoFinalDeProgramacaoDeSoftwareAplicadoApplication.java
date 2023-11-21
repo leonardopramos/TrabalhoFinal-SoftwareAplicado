545f4a8c92d363f1383cc5c885b810d841904559
leonardopramos <leonardopreczevskiramos@gmail.com>
2023-11-20 22:20:23 -0300

    conectando front com back

--- src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication.java
+++ src/main/java/br/com/trabalhoFinal/Trabalho/Final/de/Programacao/de/Software/Aplicado/TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication.java
@@ -2,12 +2,16 @@ package br.com.trabalhoFinal.Trabalho.Final.de.Programacao.de.Software.Aplicado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication {
+public class TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoFinalDeProgramacaoDeSoftwareAplicadoApplication.class, args);
 	}
+
+
 
 }
